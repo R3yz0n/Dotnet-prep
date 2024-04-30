a9286eba6f9cbb31f41ba02c67b9b0887951e79a
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-30 14:08:20 +0545

    Comment out instantiation of Swapping class and remove unused code in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-﻿// Types t = new Types();
+﻿//NOTE WE CANNOT CALL NON STATIC METHOD FROM STATIC MAIN METHOD
+
+
+// Types t = new Types();
 // Swapping s = new Swapping();
 
 // Goto g = new Goto();
 // Array a = new Array();
-Strings s = new Strings();
+// Strings s = new Strings();
+
+// Encapsulation e = new Encapsulation();
+// System.Console.WriteLine(e.Age);
+
+
+
+
