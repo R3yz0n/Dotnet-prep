c9ffdd314d2b1e642bf667926ed5ca50cfd44a9a
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-04 18:04:46 +0545

    Refactor Program.cs to fix indentation and add missing comments

--- Program.cs
+++ Program.cs
@@ -12,13 +12,55 @@
 // ------------------------------------------------------------------------------------------------
 
 //Encapsulation Example && Data Hiding
-Encapsulation account = new Encapsulation();
+// Encapsulation account = new Encapsulation();
 // Depositing and withdrawing money (encapsulation)
 // account.Deposit(1000);   // Direct access to the methodcl
 // account.Withdraw(1000);   // Direct access to the methodcl
 
 // ------------------------------------------------------------------------------------------------
+
 //Properteis & its Types
 // Properties p = new Properties();
 
+// ------------------------------------------------------------------------------------------------
+
+
+
+// ------------------------------------------------------------------------------------------------
+
+//Constructor & Deconstructor
+//Contructing
+// Constructor person1 = new Constructor("sagar", "thapa");
+
+// Explicitly calling Deconstruct
+// string fn, ln;
+// person1.Deconstruct(out string fn, out string ln);
+// System.Console.WriteLine(fn + ln);
+
+// Implicitly calling Deconstruct
+
+// Constructor person2 = new Constructor(" reyzon", "chhetri");
+
+// (string a, string b) = person2;
+// System.Console.WriteLine(a + b);
+
+// ------------------------------------------------------------------------------------------------
+
+
+
+// ------------------------------------------------------------------------------------------------
+
+//Finializer
+
+// Finalizer f = new Finalizer();
+
+// ------------------------------------------------------------------------------------------------
+
+
+
+
+// ------------------------------------------------------------------------------------------------
+
+//Static Class
+// StaticClass.IncrementCounter();
 // ------------------------------------------------------------------------------------------------
