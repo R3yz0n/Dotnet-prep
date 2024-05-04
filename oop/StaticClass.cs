bfbf91e512d928248381648df341ad610d9c8c5c
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-04 18:04:49 +0545

    Add StaticClass for demonstrating the use of static classes in OOP

---
+++ oop/StaticClass.cs
@@ -0,0 +1,27 @@
+//Static class can only have static variables,static method and static constructor
+// Cannot be instantiated however can initialize the static members with using static constructor
+// Cannot be inherited
+
+
+
+static class StaticClass
+{
+
+    public static int counter;
+    //Only called once
+    //No access modifier is allowed in static constructor
+    //Autoatically called when the class is loaded by CLR before any static members of the class are accessed.
+    static StaticClass()
+    {
+        counter = 0;
+        System.Console.WriteLine("Static constructor called");
+
+    }
+
+    public static void IncrementCounter()
+    {
+        counter++;
+        System.Console.WriteLine("Counter value: " + counter);
+    }
+
+}
