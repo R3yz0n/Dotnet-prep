ec04e83d81802c598608f3f68f990116224ac5a6
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-05 11:53:13 +0545

    Refactor StaticClass.cs for better code organization and readability

--- oop/class-types/StaticClass.cs
+++ oop/class-types/StaticClass.cs
@@ -4,12 +4,14 @@
 
 
 
+
 static class StaticClass
 {
 
     public static int counter;
     //Only called once
     //No access modifier is allowed in static constructor
+    //Cannot be Overloaded
     //Autoatically called when the class is loaded by CLR before any static members of the class are accessed.
     static StaticClass()
     {
