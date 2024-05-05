9af97e81cefed248cf9f1dc5341141d422f83049
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-05 11:21:47 +0545

    Refactor Structure.cs for better code organization and readability

---
+++ oop/Structure.cs
@@ -0,0 +1,16 @@
+//dont support Inheritance only interface
+//dont support default Constructor
+//DOnt support polymorphism
+//Value type stored in heap
+//Pass by value
+struct Structure
+{
+
+    public int num { get; set; }
+    public Structure()
+    {
+        System.Console.WriteLine("Structure Constructor");
+    }
+
+
+}
