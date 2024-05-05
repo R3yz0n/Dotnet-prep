8a70e9fa427f4a2ec103fc5270ea9e602f36cc90
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-05 11:41:30 +0545

    Add Enums.cs for defining and accessing enums in OOP

---
+++ oop/Enums.cs
@@ -0,0 +1,21 @@
+// An enum is set of named integers  (unchangeable/read-only variables).
+// It starts from 0 and incremented
+//Enums defined in a class can be accessed by using the class name.
+public enum Days
+{
+
+    Sun = 1,
+    Mon = 2
+
+}
+
+
+class Enums
+{
+
+    public enum Binary
+    {
+        no = 0,
+        yes = 1
+    }
+}
