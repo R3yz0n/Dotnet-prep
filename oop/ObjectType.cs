836c868d61eee7285ccf27f45a9e015786db39c3
R3yz0n <reyzon.thapa2020@gmail.com>
2024-05-05 10:59:23 +0545

    feat: Add ObjectType class for demonstrating boxing and unboxing in OOP

---
+++ oop/ObjectType.cs
@@ -0,0 +1,24 @@
+//Boxing When variables/objects moves from Stack to Heap
+//UnBoxing When variables/objects moves from Heap to Stack
+
+
+class ObjectType
+{
+    public ObjectType()
+    {
+
+        Object obj1 = 10;// Boxing
+        Object obj2 = obj1; //Unboxing
+
+        int intz = Convert.ToInt32(obj1); //This is unboxing
+        System.Console.WriteLine(Object.ReferenceEquals(obj1, obj2)); //True
+
+        //newly created boxed object
+        obj1 = 20;
+        System.Console.WriteLine(Object.ReferenceEquals(obj1, obj2)); //False
+        System.Console.WriteLine(obj1);
+        System.Console.WriteLine(obj2);
+
+
+    }
+}
